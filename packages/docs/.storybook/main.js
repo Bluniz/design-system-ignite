4761dae973f8f4106fb2061fc179ba811bc0eb84
Lucas Rosa <lucassantosrosa51@gmail.com>
2023-06-22 17:18:20 -0300

    add all components

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,26 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/pages/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y  "
+    
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  docs: {
+    autodocs: true
+  }
+
+};
+export default config;
