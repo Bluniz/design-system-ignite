e3ed6abeeb1168663a380345770e380208a4f0ea
Lucas Rosa <lucassantosrosa51@gmail.com>
2023-06-22 21:31:02 -0300

    change ignite-ui to aisha-ui

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { StoryObj, Meta } from '@storybook/react'
 
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@aisha-ui/react'
 import { ArrowRight } from 'phosphor-react'
 
 export default {
--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@aisha-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
